99a7337c3d115f52a1051eaad971a1f7e2e7084c
Ayobami Akingbade <akingbadefred@gmail.com>
2022-07-14 13:23:10 +0100

    Export types

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { createDriver } from "./drivers";
 
 import { IConnectionOptions } from "./types";
 
+export { Entity } from "./models/Entity";
+
 export async function introspect(connectionOptions: IConnectionOptions) {
   return await createDriver(connectionOptions.databaseType).GetDataFromServer(
     connectionOptions
